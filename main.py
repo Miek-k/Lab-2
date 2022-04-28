e3e40f23837719314c1acfe3cb374303063ba33d
Miek-k <kohp.21@ichat.sp.edu.sg>
2022-04-28 08:38:58 +0800

    another commit

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
 
 
 def display_mainmenu():
-    print("\nWe learn new language now aka we get tortured by a snake aka c++ bad")
+    print("\nWe learn new language now aka we get tortured by a snake aka c++ baddie")
     print ("Hi enter some numbers pls [use commas :P ]\n")
 
 def user_input():
